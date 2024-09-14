880da1b2c3fe578aea070e76cdc70a5ebcde1218
armandoloconte <armando.loconte95@gmail.com>
2024-09-14 17:47:36 +0200

    ok

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-source new_env/bin/activate
-nohup streamlit run main.py --server.headless=True > streamlit.log
--- stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-PROCS=`ps -o pid,command`
-PID=`echo "$PROCS" | grep streamlit | xargs | cut -d ' ' -f 1`
-
-if [ $PID ]; then
-    kill -s SIGINT $PID
-    echo "Stopped."
-fi;
